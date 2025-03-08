0fd0cc45613c1c30d3938207145ebf072a9d5706
Makunga1502 <jr5766236@gmail.com>
2025-03-07 19:56:52 -0600

    Add files via upload

---
+++ ingresarvalores.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nombre;
+    int edad;
+    
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+    
+    cout << "El nombre es " << nombre << " de edad " << edad << endl;
+    
+    return 0;
+}
